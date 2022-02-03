5b8bff37caf77b6b6afe57dd46ab918ab84936c2
unknown <rugved.bongale@somaiya.edu>
2022-02-03 23:56:38 +0530

    Attached mousedown,up,move events to canvas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import rough from 'roughjs/bundled/rough.esm';
 
 const generator = rough.generator()
 
 function App() {
+
+  const [elements, setElements] = useState([])
+  const [drawing, setDrawing] = useState(false)
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas")
@@ -16,11 +19,29 @@ function App() {
     roughCanvas.draw(line)
   })
 
+  const handleMouseDown = (event) => {
+    setDrawing(true)
+  }
+
+  const handleMouseMove = (event) => {
+    if(!drawing) return
+
+    const { clientX, clientY } = event
+    console.log(clientX, clientY)
+  }
+
+  const handleMouseUp = (event) => {
+    setDrawing(false)
+  }
+
   return (
     <canvas 
       id="canvas"
       width={window.innerWidth}
       height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
     >
       Canvas
     </canvas>
