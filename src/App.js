d2227172283c621106f78d974a38dc80e91c6fe1
unknown <rugved.bongale@somaiya.edu>
2022-02-04 20:33:14 +0530

    Done with implementation of resizing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,12 @@ function createElement(id, x1, y1, x2, y2, type) {
   }
 }
 
-const isWithinElement = (x, y, element) => {
+const nearPoint = (x, y, x1, y1, name) => {
+  const offset = 10
+  return Math.abs(x - x1) < offset && Math.abs(y - y1) < offset ? name : null
+}
+
+const positionWithinElement = (x, y, element) => {
   const { x1, x2, y1, y2, type } = element
   
   switch(type) {
@@ -27,18 +32,24 @@ const isWithinElement = (x, y, element) => {
       const maxX = Math.max(x1, x2)
       const minY = Math.min(y1, y2)
       const maxY = Math.max(y1, y2)
-      console.log(x,y)
-      console.log(minX,minY,maxX,maxY)
-      console.log(x >= minX && x <= maxX && y >= minY && y <= maxY)
-      return x >= minX && x <= maxX && y >= minY && y <= maxY
+     
+      const topLeft = nearPoint(x, y, minX, minY, "tl")
+      const topRight = nearPoint(x, y, maxX, minY, "tr")
+      const bottomLeft = nearPoint(x, y, minX, maxY, "bl")
+      const bottomRight = nearPoint(x, y, maxX, maxY, "br")
+      const insideRect = x >= minX && x <= maxX && y >= minY && y <= maxY ? "inside" : null 
+
+      return topLeft || topRight || bottomLeft || bottomRight || insideRect
     case "line":
       console.log("line for selection")
       const a = { x: x1, y: y1 }
       const b = { x: x2, y: y2 }
       const c = { x: x, y: y }
       const offset = distance(a, b) - (distance(a, c) + distance(b, c))
-      console.log("abc offset")
-      return Math.abs(offset) < 1
+      const start = nearPoint(x, y, x1, y1, "start")
+      const end = nearPoint(x, y, x2, y2, "end")
+      const insideLine = Math.abs(offset) < 1  ? "inside" : null
+      return start || end || insideLine
   }
 }
 
@@ -47,7 +58,59 @@ function distance(a, b){
 }
 
 function getElementAtPosition(x, y, elements) {
-  return elements.find(element => isWithinElement(x, y, element))
+  return elements.map(element => ({ ...element, position: positionWithinElement(x, y, element)}))
+  .find(element => element.position !== null)
+}
+
+function adjustElementCoordinates(element){
+  const { type, x1, y1, x2, y2 } = element
+  switch(type){
+    case "rectangle":
+      const minX = Math.min(x1, x2)
+      const maxX = Math.max(x1, x2)
+      const minY = Math.min(y1, y2)
+      const maxY = Math.max(y1, y2)
+      return { x1: minX, y1: minY, x2: maxX, y2: maxY }
+    case "line":
+      if((x1 < x2 || x1 === x2) && y1 < y2){
+        return { x1, y1, x2, y2 }
+      }
+      return { x1: x2, y1: y2, x2: x1, y2: y1 }
+  }
+}
+
+function cursorForPosition(position){
+  switch(position) {
+    case "tl":
+    case "br":
+    case "start":
+    case "end":
+      return "nwse-resize"
+    case "tr":
+    case "bl":
+      return "nesw-resize"
+    default:
+      return "move"
+  }
+}
+
+function resizedCoordinates(clientX, clientY, position, coordinates) {
+  const { x1, y1, x2, y2 } = coordinates
+  switch(position) {
+    case "tl":
+    case "start":
+      return { x1: clientX, y1: clientY, x2, y2 }
+    case "tr":
+      return { x1, y1: clientY, x2: clientX, y2 }
+    case "bl":
+      return { x1: clientX, y1, x2, y2: clientY }
+    case "br":
+    case "end":
+      return { x1, y1, x2: clientX, y2: clientY }
+    default:
+      return null
+  }
+
 }
 
 function App() {
@@ -73,12 +136,18 @@ function App() {
 
     if(tool === "selection"){
       const element = getElementAtPosition(clientX, clientY, elements)
-      console.log("element at selection is: ")
-      console.log(element)
+      
       if(element) {
         const offsetX = clientX - element.x1
         const offsetY = clientY - element.y1
-        setAction("moving")
+
+        if(element.position === "inside"){
+          setAction("moving")
+        }
+        else{
+          console.log('setting action to resizing')
+          setAction("resizing")
+        }
         setSelectedElement({...element, offsetX, offsetY})
       }
     }
@@ -87,6 +156,7 @@ function App() {
       const element = createElement(id, clientX, clientY, clientX, clientY, tool)
       setElements(prevState => [...prevState, element])
       setAction("drawing")
+      setSelectedElement(element)
 
     }
   }
@@ -103,7 +173,8 @@ function App() {
     const { clientX, clientY } = event
 
     if(tool === "selection"){
-      event.target.style.cursor = getElementAtPosition(clientX, clientY, elements) ? "move" : "default"
+      const element = getElementAtPosition(clientX, clientY, elements)
+      event.target.style.cursor = element ? cursorForPosition(element.position) : "default"
     }
     
     if(action === "drawing"){
@@ -121,10 +192,25 @@ function App() {
       const newY1 = clientY - offsetY
       updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type)
     }
+    else if(action === "resizing") {
+      console.log("action resizing, selectedElement:")
+      console.log(selectedElement)
+      const { id, type, position, ...coordinates } = selectedElement
+      const { x1, y1, x2, y2 } = resizedCoordinates(clientX, clientY, position, coordinates)
+      updateElement(id, x1, y1, x2, y2, type)
+    }
 
   }
 
   const handleMouseUp = (event) => {
+    const index = selectedElement.id
+    const { id, type } = elements[index]
+
+    if(action === "drawing" || action === "resizing") {
+      const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index])
+      updateElement(id, x1, y1, x2, y2, type)
+    }
+
     setAction("none")
     setSelectedElement(null)
   }
