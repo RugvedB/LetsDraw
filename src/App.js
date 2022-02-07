4fe97dc0618154a253b65fceb0a2ba297e17e54d
unknown <rugved.bongale@somaiya.edu>
2022-02-07 18:35:57 +0530

    some basic styling

--- src/App.js
+++ src/App.js
@@ -527,9 +527,9 @@ function App() {
       </div>
 
       <div style={{ position: "fixed", top: 0, right: 0, padding: "10px" }}>
-        <button onClick={() => undo()}>Undo (Ctrl + Z)</button>
-        <button onClick={() => redo()}>Redo (Ctrl + Y)</button>
-        <button onClick={() => clearScreen()}>Clear Screen</button>
+        <button style={{ marginRight:"5px" }} onClick={() => undo()}>Undo (Ctrl + Z)</button>
+        <button style={{ marginRight:"5px" }} onClick={() => redo()}>Redo (Ctrl + Y)</button> 
+        <button style={{ backgroundColor: "red", color:"white" }} onClick={clearScreen}>Clear Screen</button>
       </div>
 
       {
